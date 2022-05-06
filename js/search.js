a241f9854a1efc4edd3585e59abba60ba9847804
SkyTheNerd <bentleyy40@gmail.com>
2022-05-06 15:45:26 -0400

    FINALLY GOT THE SEARCH TO WORK
    WOOOO
    AFTER MONTHS OF TRYING
    LETS GOOOOO

--- js/search.js
+++ js/search.js
@@ -1,15 +1,7 @@
 const urlq = new URLSearchParams(window.location.search);
 const q = urlq.get("q");
 //alert("hello world!")
-alert("search.js says: "+q)
-function handleData(d){
-	const vm = "search_metadata";
-	document.getElementById("seterm").textContent = `Searched for ${d[vm]["query"]}, corrected to ${d[vm]["corrected_query"]}, ${d[vm]["estimated_results"]} results`;
-	const v = d["videos"];
-	for(var i in v){
-		var div = document.createElement("div");
-		div.setAttribute("onclick", `location.href="watch.html?id=${v[i]["id"]}"`);
-		div.innerHTML = `<h4>${v[i]["title"]}</h4><p class="vdesc">${v[i]["description"]}</p><p class="sub_d">${v[i]["author"]} - ${v[i]["metadata"]["published"]} - ${v[i]["metadata"]["duration"]["simple_text"]}</p>`;
-		document.getElementById("vwrap").appendChild(div);
-	}
-}
+window.onload = function() {  
+	document.getElementById('query').innerHTML = "https://www.bing.com/search?q="+q;
+};
+
--- js/tile.js
+++ js/tile.js
@@ -1,7 +1,5 @@
 function Search(){
 	var q = document.getElementById("yt_term").value;
-	alert('If you see this, You are problary a beta tester!')
-	alert('This is what you searched up!')
 	if(q!=""){
 		location.href="search.html?q="+q;
 	}
