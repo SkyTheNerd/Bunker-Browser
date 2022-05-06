a9dd22759f84e65a028e3feecb02074969332d82
SkyTheNerd <bentleyy40@gmail.com>
2022-05-06 16:39:01 -0400

    Added ability to search URL's
    It finally happened

--- js/search.js
+++
@@ -1,7 +0,0 @@
-const urlq = new URLSearchParams(window.location.search);
-const q = urlq.get("q");
-//alert("hello world!")
-window.onload = function() {  
-	document.getElementById('query').innerHTML = "https://www.bing.com/search?q="+q;
-};
-
--- js/tile.js
+++
@@ -1,6 +0,0 @@
-function Search(){
-	var q = document.getElementById("yt_term").value;
-	if(q!=""){
-		location.href="search.html?q="+q;
-	}
-}
